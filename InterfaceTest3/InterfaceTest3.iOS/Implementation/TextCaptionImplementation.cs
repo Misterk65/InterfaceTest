98c6874e1b9a7454accabdbd3fd586f9266e1ad6
Karsten Peucker <karsten.peucker@icloud.com>
2018-11-09 14:22:47 +0900

    Added IOS

---
+++ InterfaceTest3/InterfaceTest3.iOS/Implementation/TextCaptionImplementation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using InterfaceTest3.iOS.Implementation;
+using InterfaceTest3.Interfaces;
+using UIKit;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(TextCaptionImplementation))]
+
+namespace InterfaceTest3.iOS.Implementation
+{
+    class TextCaptionImplementation : ITextCaption
+    {
+        public string GetText(string x)
+        {
+
+            if (x != "schlecht Fotze lecken ! :-)))")
+            {
+                x = "schlecht Fotze lecken ! :-)))";
+            }
+            else
+            {
+                x = "Drei Mann in einem Raum...";
+            }
+
+            return (x);
+        }
+    }
+}
